13f288750acd0213382a4711758e84384fcaffc8
GCKulak <cade.kulak@Knights.ucf.edu>
2021-02-28 11:41:14 -0500

    First commit

---
+++ Assets/Scripts/escape.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escape : MonoBehaviour
+
+{    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
