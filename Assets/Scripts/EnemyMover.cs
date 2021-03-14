4855f06458fccdd5cd4a3a26ab68dcde022accac
GCKulak <cade.kulak@Knights.ucf.edu>
2021-03-14 09:13:13 -0400

    Second Commit

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    // Transforms to act as start and end markers for the journey.
+    public Transform startMarker;
+    public Transform endMarker;
+
+    // Movement speed in units/sec.
+    public float speed = 1.0F;
+
+  
+
+    void Start()
+    {
+      
+    }
+
+    // Follows the target position like with a spring
+    void Update()
+    {
+        // Distance moved = time * speed.
+       
+        // Set our position as a fraction of the distance between the markers and pingpong the movement.
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(Time.time * speed, 1));
+    }
+}
---
+++ Assets/Scripts/PlayerScript2.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript2 : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text score;
+
+    public Text life;
+    
+    public Text winText;
+
+    private int lifeValue = 3;
+
+    private int scoreValue = 0;
+
+    public AudioSource backSong;
+    public AudioSource winSong;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = "Score: " + scoreValue.ToString();
+        life.text = "Lives: " + lifeValue.ToString();
+        SetLevel();
+        SetCountText();
+        winText.text = "";
+      
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            SetLevel();
+            SetCountText();
+        }
+
+        else if (collision.collider.tag == ("Enemy"))
+        {
+            lifeValue -= 1;
+            life.text = "Lives: " + lifeValue.ToString();
+            Destroy(collision.collider.gameObject);
+            SetCountText();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        
+       if (scoreValue == 8)
+        {
+            winText.text = "You Win! - Game Made By Kulak";
+            backSong.Stop();
+            winSong.Play();
+        }
+
+        else if (lifeValue <= 0)
+        {
+            winText.text = "YOU DIED";
+            Destroy(gameObject);
+            
+        }
+    }
+    void SetLevel()
+    {
+
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector2(52, 0);
+            lifeValue = 3;
+            life.text = "Lives: " + lifeValue.ToString();
+
+        }
+        
+    }
+}
